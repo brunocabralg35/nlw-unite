a85866a505f85733bbde6bf07a2fd189d6486323
Bruno Cabral <brunocabral035@gmail.com>
2024-04-04 15:29:35 -0300

    feat: add check-in route

---
+++ server/src/routes/check-in.ts
@@ -0,0 +1,43 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function checkIn(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/attendees/:attendeeId/check-in",
+    {
+      schema: {
+        params: z.object({
+          attendeeId: z.coerce.number().int(),
+        }),
+        response: {
+          201: z.object({
+            attendeeId: z.number(),
+          }),
+        },
+      },
+    },
+    async (req, reply) => {
+      const { attendeeId } = req.params;
+
+      const attendeeCheckIn = await prisma.checkIn.findUnique({
+        where: {
+          attendeeId,
+        },
+      });
+
+      if (attendeeCheckIn !== null) {
+        throw new Error("Attendee already checked in");
+      }
+
+      await prisma.checkIn.create({
+        data: {
+          attendeeId,
+        },
+      });
+
+      return reply.status(201).send({ attendeeId });
+    }
+  );
+}
